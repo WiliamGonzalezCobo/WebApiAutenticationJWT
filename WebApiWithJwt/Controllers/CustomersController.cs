6c62bba601ef1acd87feb1511603a7375b3c5a09
William Fabian Gonzalez Cobo <wifagoco@gmail.com>
2019-07-29 19:09:05 -0500

    version inicial del codigo por mejorar del web api con jwt

---
+++ WebApiWithJwt/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiWithJwt.Controllers
+{
+    /// <summary>
+    /// customer controller class for testing security token
+    /// </summary>
+    [Authorize]
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        [Route("getid")]
+        public IHttpActionResult GetId(int? id)
+        {
+            var customerFake = "customer-fake";
+            return Ok(customerFake);
+        }
+
+        [HttpGet]
+        [Route("getall")]
+        public IHttpActionResult GetAll()
+        {
+            var customersFake = new string[] { "customer-1", "customer-2", "customer-3" };
+            return Ok(customersFake);
+        }
+    }
+}
